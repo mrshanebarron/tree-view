0bbe0852de71c83b0eadbf297b95b03b2df9b148
Shane Barron <shane@Shanes-MacBook-Pro.local>
2025-12-20 23:47:02 -0500

    Add missing public properties and methods to Livewire component

--- src/Livewire/TreeView.php
+++ src/Livewire/TreeView.php
@@ -6,6 +6,25 @@ use Livewire\Component;
 
 class TreeView extends Component
 {
+    public array $items = [];
+    public array $expanded = [];
+    public ?string $selected = null;
+    public bool $showIcons = true;
+
+    public function toggle(string $id): void
+    {
+        if (in_array($id, $this->expanded)) {
+            $this->expanded = array_diff($this->expanded, [$id]);
+        } else {
+            $this->expanded[] = $id;
+        }
+    }
+
+    public function select(string $id): void
+    {
+        $this->selected = $id;
+    }
+
     public function render()
     {
         return view('sb-tree-view::livewire.tree-view');
